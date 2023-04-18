051d1029a35c52dc6fc482ab664b155906492f32
yAerosmith <larata365@gmail.com>
2023-04-18 08:01:36 -0600

    Instalación de Laravel y TailwindCSS

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perú</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1 class="font-bold text-12">
+        Hola negros
+    </h1>
+</body>
+</html>
