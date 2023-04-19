bafce0c564011767a6d91ebdba01ff2f2ee0bbc5
yAerosmith <larata365@gmail.com>
2023-04-18 20:27:28 -0600

    Corrección - CSS en index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Index</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <h1 class="bg-indigo-400  font-serif">Belle Vie</h1>
