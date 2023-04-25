84643cab50c47d8c7653c0a3a84a8bdc914760f3
FloppaCoder <87096987+FloppaCoder@users.noreply.github.com>
2023-04-25 08:56:04 -0600

    Navbar Responsive Correciones

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,14 +1,11 @@
-
-
-<!-- Eliminar "rounded-2xl" para lograr el Navbar cuadrado (y hacer los respectivos cambios en el componente que se requiera) -->
-    <nav class='flex justify-between rounded-2xl bg-[#f87171] drop-shadow-md px-9 py-6 list-none '>
-        <img src="{{ asset('img/favicon.png') }}" alt="Logo" class='h-9 cursor-pointer'/>
-        <div class="md:flex hidden items-center gap-10 text-white md:my-0 my-7">
+<nav className='flex justify-between rounded-2xl bg-black drop-shadow-md px-9 py-6 list-none '>
+        <img src={Logo} alt="Logo" className='h-9 cursor-pointer'/>
+        <div className="md:flex hidden items-center gap-10 text-white md:my-0 my-7">
           <NavLinks />
         </div>
-        <IoIosMenu class="md:hidden text-white text-2xl cursor-pointer my-2 list-none" onClick={handleMenuClick} />
+        <IoIosMenu className="md:hidden text-white text-2xl cursor-pointer my-2 list-none" onClick={handleMenuClick} />
       </nav>
       {showMenu && (
-        <div class="md:hidden flex flex-col items-center justify-center gap-5 py-3 my-1 text-white list-none bg-black rounded-t-lg rounded-2xl">
+        <div className="md:hidden flex flex-col items-center justify-center gap-5 py-3 my-1 text-white list-none bg-black rounded-t-lg rounded-2xl">
           <NavLinks />
         </div>
---
+++ resources/views/components/navlinks.blade.php
@@ -0,0 +1,17 @@
+
+    <script>
+        
+    </script>
+
+
+
+        
+
+            <script>
+                Links.map(Link, i) => 
+                <li key={i} className="font-semibold hover:text-terc">
+                    <a href={Link.link}>{Link.name}</a>
+                </li>
+            </script>
+
+
