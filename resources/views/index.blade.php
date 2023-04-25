3c6a28221c5f0c6910e81d2165ec02de87491c18
Viiictor01 <87097376+Viiictor01@users.noreply.github.com>
2023-04-25 08:20:47 -0600

    slider mal hecho😒

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,47 +10,28 @@
 <body class="bg-[#ECF0F1]">
     <nav></nav>
 
-<div class="max-w-2xl mx-auto">
-	<div id="default-carousel" class="relative" data-carousel="static">
-        <!-- Carousel wrapper -->
-        <div class="overflow-hidden relative h-56 rounded-lg sm:h-64 xl:h-80 2xl:h-96">
-            <!-- Item 1 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('img/Rancho2.jpg') }}" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
-            </div>
-            <!-- Item 2 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('img/Rancho1.jpg') }}" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
-            </div>
-            <!-- Item 3 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('img/Rancho2.jpg') }}" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
-            </div>
+    <div class="relative object-center max-w-2xl mx-auto">
+        <div class="overflow-x-auto whitespace-nowrap">
+          <div class="inline-block px-4">
+            <img src="{{ asset('img/Rancho2.jpg') }}" class="" alt="">
+          </div>
+          <div class="inline-block px-4 ">
+            <img src="{{ asset('img/Rancho1.jpg') }}" class="" alt="">
+          </div>
+          <div class="inline-block px-4">
+            <img class="" src="{{ asset('img/Rancho2.jpg') }}"class="" alt="">
+          </div>
         </div>
-        <!-- Slider indicators -->
-        <div class="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-        </div>
-        <!-- Slider controls -->
-        <button type="button" class="flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
-            <span class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
-                <span class="hidden">Previous</span>
-            </span>
+        <button class="absolute top-1/2 left-0 transform -translate-y-1/2 px-4 text-white bg-[#E95F4A] rounded-md hover:bg-gray-800" onclick="document.querySelector('.overflow-x-auto').scrollBy({left: -600, behavior: 'smooth'})">
+          &lt;
         </button>
-        <button type="button" class="flex absolute top-0 right-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-next>
-            <span class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
-                <span class="hidden">Next</span>
-            </span>
+        <button class="absolute top-1/2 right-0 transform -translate-y-1/2 px-4 text-white bg-[#E95F4A] rounded-md hover:bg-gray-800" onclick="document.querySelector('.overflow-x-auto').scrollBy({left: 600, behavior: 'smooth'})">
+          &gt;
         </button>
     </div>
-    <script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>
-</div>
+      
     
-<hr><hr>
+<br>
 
     <h2 class="text-2xl font-bold tracking-tight text-[#E95F4A] flex items-center justify-center">Recientemente Agregados</h2>
     <br>
@@ -84,7 +65,7 @@
     </div>
 
     <br>
-
+    <hr><hr>
     <h2 class="text-2xl font-bold tracking-tight text-[#E95F4A] flex items-center justify-center">Busca en tu área</h2>
     <br>
 
