71d3d5a750acaccd27435af643f3d1dd1bbcb290
Vinic`-` <viictorgarcia01@gmail.com>
2023-04-27 21:57:34 -0600

    Slider ok

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,26 +15,26 @@
 
 <a href="{{ route('profile', $user) }}"></a>
 
-    <div class="py-8 relative object-center max-w-2xl mx-auto">
-        <div class="overflow-x-auto whitespace-nowrap">
-          <div class="inline-block px-4">
-            <img src="{{ asset('img/Rancho2.jpg') }}" class="" alt="">
-          </div>
-          <div class="inline-block px-4 ">
-            <img src="{{ asset('img/Rancho1.jpg') }}" class="" alt="">
-          </div>
-          <div class="inline-block px-4">
-            <img class="" src="{{ asset('img/Rancho2.jpg') }}"class="" alt="">
-          </div>
+<div class="max-w-2xl mx-auto">
+
+	<div id="default-carousel" class="relative" data-carousel="static">
+        <div class="overflow-hidden relative h-56 rounded-lg sm:h-64 xl:h-80 2xl:h-96">
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                <span class="absolute top-1/2 left-1/2 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 sm:text-3xl ">Primer Slide</span>
+                <img src="{{ asset('img/Rancho2.jpg') }}" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
+            </div>    
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                <img src="{{ asset('img/Rancho1.jpg') }}" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
+            </div>    
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                <img src="{{ asset('img/Rancho2.jpg') }}" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
+            </div>
         </div>
-        
-        
         <div class="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
             <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 1" data-carousel-slide-to="0"></button>
             <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
             <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
         </div>
-       
         <button type="button" class="flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
             <span class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30  group-focus:ring-4 group-focus:ring-white  group-focus:outline-none">
                 <svg class="w-5 h-5 text-white sm:w-6 sm:h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
@@ -85,6 +85,7 @@
     </div>
 
     <br>
+
     <hr><hr>
     <h2 class="text-2xl font-bold tracking-tight text-[#E95F4A] flex items-center justify-center">Busca en tu área</h2>
     <br>
