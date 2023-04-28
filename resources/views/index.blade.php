568be83aca20ec09699e24e4bbeabda94b5b9c10
Vinic`-` <viictorgarcia01@gmail.com>
2023-04-27 22:28:22 -0600

    index al -5185121%

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,9 +14,8 @@
 <x-navbar></x-navbar>
 
 <a href="{{ route('profile', $user) }}"></a>
-
+<br>    
 <div class="max-w-2xl mx-auto">
-
 	<div id="default-carousel" class="relative" data-carousel="static">
         <div class="overflow-hidden relative h-56 rounded-lg sm:h-64 xl:h-80 2xl:h-96">
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
@@ -97,23 +96,24 @@
     <br>
 
     
-<footer class="bg-[#E95F4A]  shadow ">
+<footer class="bg-[#E95F4A]  shadow  ">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="#" class="flex items-center mb-4 sm:mb-0">
-                <img src="{{ asset('img/favicon.png') }}" class="h-8 mr-3" alt="Flowbite Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Belle Vie</span>
+        <div class="sm:flex sm:items-center sm:justify-between ">
+            <a href="#" class="flex items-center mb-4 sm:mb-0 ">
+                <img src="{{ asset('img/favicon.png') }}" class="h-8 mr-3 grid  place-items-center" alt="Flowbite Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white text-center ">Belle Vie</span>
             </a>
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-white ">
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-white">
                 <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6 ">Sobre Nosotros</a>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 ">Sobre Nosotros </a>
                 </li>
+                
                 <li>
-                    <a href="https://github.com/yAerosmith/Belle-Vie" class="mr-4 hover:underline md:mr-6">Respositorio</a>
+                    <a href="https://github.com/yAerosmith/Belle-Vie" class="flex flex-wrap items-center justify-center mr-4 hover:underline md:mr-6">| Respositorio</a>
                 </li>
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-white sm:text-center ">© 2023 <a href="https://flowbite.com/" class="hover:underline">Belle Vie™</a></span>
+        <span class="block text-sm text-white sm:text-center ">© 2023 <a href="" class="hover:underline">Belle Vie™</a></span>
     </div>
 </footer>
 </body>
