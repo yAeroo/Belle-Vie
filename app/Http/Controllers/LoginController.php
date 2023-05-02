38846b86439bae46a5aafe7e3e44c1801321d324
yAerosmith <larata365@gmail.com>
2023-05-01 21:39:55 -0600

    Resolviendo error login -_-

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -22,6 +23,10 @@ class LoginController extends Controller
 
             return redirect('index');
         }
+
+        throw ValidationException::withMessages([
+            'email' => __('auth.failed')
+        ]);
     }
 
     public function logout(Request $request){
