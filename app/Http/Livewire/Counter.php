6fcc6f1fc7e5f7ac05088591ad76175af37b0402
Vinic`-` <viictorgarcia01@gmail.com>
2023-05-06 23:31:07 -0600

    buscador?
    Codigo de buscador, falta el mostar resultados y agregarlo  a la vista search

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -3,11 +3,24 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\User;
 
 class Counter extends Component
 {
+    public $usuarios;
+
+    public function BuscarDatosForm()
+    {
+        // Emito el evento para el padre
+        $this->emit('terminoBusquedaGeneral', $this->termino, $this->usuarios);
+    }
+
     public function render()
     {
-        return view('livewire.counter');
-    }
+        $usuarios = User::where('rol', '<>', 'admin');
+
+        return view('livewire.buscador', [
+            'users' => $usuarios
+
+        ]);}
 }
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -4,6 +4,8 @@
 
 <h1 class="">Que tal soy searc</h1>
 
+
+
 <img src="https://i.ytimg.com/vi/l3WLOrWBrro/maxresdefault.jpg" alt="" width="40%">
 <br><br><br><br><br><br>
 
