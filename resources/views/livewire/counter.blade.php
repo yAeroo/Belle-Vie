bb0e41771120117ad23b3d22145644f95037d36e
ineedoritos <87097376+Viiictor01@users.noreply.github.com>
2023-05-09 11:58:36 -0600

    Search (Si sirve)
    AERO hizo el input del SEARCH aesthetic, gracias AERO por tanto no haria nada sin ti

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,18 +1,26 @@
 
-<div class=" bg-[#f87171]">
-    <h3 class="h1 alert alert-success">Busca Usuarios</h3>
+<div class="flex justify-center items-center flex-col">
+    <div>
     <form>
-        <input class="form-control" wire:model="searchTerm" type="text" placeholder="ingresa">
+        <div id="input-cont" class="flex items-center border-solid border-2 border-black rounded-lg bg-white">
+            <span class="icon inline p-1"><x-icon name="magnifying-glass" color="#9ca3af"/></span>
+        <input class="form-control rounded-lg px-3 bg-white focus:outline-none" wire:model="searchTerm" type="text" placeholder="Estas buscando algo?">
+        </div>
     </form><br>
-
+    </div>
     <ul class="list-group">
         @if($users && $users->count() > 0)
         @foreach($users as $user)
-        <a class="list-group-item" href="{{ route('profile', $user) }}">{{$user->name}}</a>
+        <a class="list-group-item grid text-center max-w-md mx-auto bg-[#f87171] rounded-xl shadow-md overflow-hidden md:max-w-2xl m-5 p-5" href="{{ route('profile', $user) }}">            <img
+            src="{{ asset('storage') . '/pfp/' . $user->profile_pic }}"
+            class=" w-1/6  rounded-full transition-all border-secu border-solid border-4"
+            alt="profile-pic">
+{{$user->name}}</a>
         @endforeach
         @else
         <p class="text-info"></p>
         @endif
     </ul>
-</div>
+</div> 
+
 
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,15 +1,12 @@
 @extends('layout.AppLayout') @section('title') Search @endsection 
 
 @section('content')
+<x-navbar />
 
-
-<div class="container">
-    <div class="row">
         <div class="col-md-4   h-screen place-items-center">
             @livewire('counter')
         </div>
-    </div>
-</div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
 @livewireScripts
  
