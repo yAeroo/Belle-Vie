64c145b37ca12156ac7cef42dd33f0769f8dc46b
Vinic`-` <viictorgarcia01@gmail.com>
2023-05-08 18:28:30 -0600

    Solo controlador y vista del Admin
    para jonny , falta habilitar la vista y crear la ruta

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,6 +1,7 @@
 <div>
+    <h3 class="h1 alert alert-success">Busca Usuarios</h3>
     <form>
-        <input class="form-control" wire:model="searchTerm" type="text" placeholder="What are you searching for...">
+        <input class="form-control" wire:model="searchTerm" type="text" placeholder="ingresa">
     </form><br>
 
     <ul class="list-group">
@@ -9,8 +10,9 @@
         <li class="list-group-item">{{$user->name}}</li>
         @endforeach
         @else
-        <p class="text-info">No users found using your search query</p>
+        <p class="text-info"></p>
         @endif
 
     </ul>
 </div>
+
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -7,7 +7,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-4 offset-md-4">
-            <h3 class="h1 alert alert-success">Busca Usuarios</h3>
+
         </div>
     </div>
 </div>
