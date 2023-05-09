c40913a50c76387d05efa972f2e5ad94195445f0
Viiictor01 <87097376+Viiictor01@users.noreply.github.com>
2023-05-09 08:16:07 -0600

    Search FUNCIONAL-
    que tal soy el search

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,4 +1,5 @@
-<div>
+
+<div class=" bg-slate-300">
     <h3 class="h1 alert alert-success">Busca Usuarios</h3>
     <form>
         <input class="form-control" wire:model="searchTerm" type="text" placeholder="ingresa">
@@ -7,12 +8,11 @@
     <ul class="list-group">
         @if($users && $users->count() > 0)
         @foreach($users as $user)
-        <li class="list-group-item">{{$user->name}}</li>
+        <a class="list-group-item" href="{{ route('profile', $user) }}">{{$user->name}}</a>
         @endforeach
         @else
         <p class="text-info"></p>
         @endif
-
     </ul>
 </div>
 
