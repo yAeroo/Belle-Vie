898fc406ca239e74641da1597729cc4f19fd13f2
FloppaCoder <87096987+FloppaCoder@users.noreply.github.com>
2023-05-10 00:35:53 -0600

    Vista de Ranchos 100% completada

--- resources/views/showPlace.blade.php
+++ resources/views/showPlace.blade.php
@@ -11,7 +11,7 @@
               <img class="h-23 object-cover md:w-48 rounded-full" src="{{ asset('img/example.png') }}" alt="Rancho 1">
             </div>
             <div class="w-2/3">
-              <div class="uppercase tracking-wide text-lg  text-[#e95f4a]  font-semibold">Gregorio Ramirez</div>
+              <div class="uppercase tracking-wide text-lg  text-[#e95f4a]  font-semibold">Gregorio Ramirez (Propietario)</div>
               <p class="mt-2 p-2 text-slate-500">Xx_Tilin69_xX</p>
             </div>
           </div>
@@ -20,7 +20,7 @@
 
       <div class="p-8 flex flex-col items-center justify-center font-semibold">
         <div class="tracking-wide text-2xl  text-[#e95f4a]">Rancho El Pepe</div>
-        <p class="mt-3 p-2 text-slate-500 text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis illo eius libero commodi enim error inventore labore. Quasi explicabo nisi labore voluptate quam eaque officia, ipsam, sed, ad illum laudantium!</p>
+        <p class="mt-3 p-2 text-slate-500 text-justify">El Rancho "El Pepe" es una propiedad impresionante que ha sido cuidadosamente diseñada para ofrecer un ambiente de vida en el campo con comodidades modernas. La finca se encuentra en una ubicación privilegiada con vistas impresionantes a las montañas circundantes, lo que la convierte en un refugio ideal para aquellos que buscan escapar del ajetreo y el bullicio de la ciudad.</p>
         <button class="bg-prim mt-4 text-white py-2 px-2 rounded-lg focus:outline-none transition duration-300 ease-in-out">Reservar Rancho</button>
       </div>
   
@@ -28,49 +28,29 @@
 
       <div class="bg-prim w-screen drop-shadow-lg overflow-hidden my-12">
         <div class="px-6 py-8">
-          <div class="uppercase text-white text-shadow font-bold text-lg pb-6">Reseñas</div>
+          <div class="uppercase text-white text-shadow font-bold text-lg pb-1 ">Reseñas</div>
 
-            <div class="flex items-center px-4 py-4  bg-white">
-              <div class="w-2/3">
-                <div class="flex place-items-center">
-                  <div class="w-1/1">
-                    <img class="h-23 object-cover md:w-48 rounded-full" src="{{ asset('img/example.png') }}" alt="imgPerfil">
-                  </div>
-                  <div class="uppercase tracking-wide text-lg  text-[#e95f4a]  font-semibold ml-3">Gregorio Ramirez</div>
-                </div>
-                <p class="mt-2 p-2 text-slate-500">Emm, el rancho esta bueno we (texto relleno alv texto relleno alv texto relleno alv).</p>
+            <div class="flex px-5 py-6 mt-6 bg-white mx-auto">
+              <div class="w-12 absolute">
+                <img class="h-23 object-cover md:w-48 rounded-full" src="{{ asset('img/catHDpfp.jpg') }}" alt="ImgPerfil">
+              </div>
+              <div class="w-3/1 ml-12">
+                <div class=" tracking-wide text-lg text-[#e95f4a] font-semibold ml-3">ThePianistCat88</div>
+                <p class=" p-2 text-slate-500 ml-1 text-md tracking-wide text-left">El rancho cuenta con instalaciones de primera clase para criar y mantener ganado, así como para la agricultura y la producción de alimentos.</p>
+              </div>
+            </div>
+
+            <div class="flex px-5 py-6 mt-6 bg-white mx-auto">
+              <div class="w-12 absolute">
+                <img class="h-23 object-cover md:w-48 rounded-full" src="{{ asset('img/example.png') }}" alt="ImgPerfil">
+              </div>
+              <div class="w-3/1 ml-12">
+                <div class=" tracking-wide text-lg text-[#e95f4a] font-semibold ml-3">xGato_weon_69</div>
+                <p class=" p-2 text-slate-500 ml-1 text-md tracking-wide text-left">Ermano tenei el meo rancho recomendadisimo 10/10</p>
               </div>
             </div>
           
-            <div class="flex items-center px-4 py-4 mt-6 bg-white">
-              <div class="w-1/3">
-                <img class="h-23 object-cover md:w-48 rounded-full" src="{{ asset('img/example.png') }}" alt="ImgPerfil">
-              </div>
-              <div class="w-2/3">
-                <div class="uppercase tracking-wide text-lg  text-[#e95f4a]  font-semibold">Gregorio Ramirez</div>
-                <p class="mt-2 p-2 text-slate-500">Emm, el rancho esta bueno we (texto relleno alv texto relleno alv texto relleno alv).</p>
-              </div>
-            </div>
-
-            <div class="flex px-4 py-4 mt-6 bg-white mx-auto">
-              <div class="w-1/6">
-                <img class="h-23 object-cover md:w-48 rounded-full" src="{{ asset('img/example.png') }}" alt="ImgPerfil">
-              </div>
-              <div class="w-2/3">
-                <div class="uppercase tracking-wide text-lg  text-[#e95f4a]  font-semibold ml-3">Gregorio Ramirez</div>
-                <p class="mt-2 p-2 text-slate-500 ml-1 text-md tracking-normal">Emm, el rancho esta bueno we (texto relleno alv texto relleno alv texto relleno alv).</p>
-              </div>
-            </div>
-
-            <div class="flex px-4 py-4 mt-6 bg-white mx-auto">
-              <div class="w-1/6">
-                <img class="h-23 object-cover md:w-48 rounded-full" src="{{ asset('img/example.png') }}" alt="ImgPerfil">
-              </div>
-              <div class="w-2/3">
-                <div class="uppercase tracking-wide text-lg  text-[#e95f4a]  font-semibold ml-3">Gregorio Ramirez</div>
-                <p class="mt-2 p-2 text-slate-500 ml-1 text-sm tracking-normal">Emm, el rancho esta bueno we (texto relleno alv texto relleno alv texto relleno alv).</p>
-              </div>
-            </div>
+            
 
           </div>
         </div>
