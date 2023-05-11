1629622588afa36d9f84500d264af290ac660bda
Vinic`-` <viictorgarcia01@gmail.com>
2023-05-10 20:46:59 -0600

    Impresion datatos search (falta animaciones)

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,4 +1,3 @@
-
 <div class="flex justify-center items-center flex-col">
     <div>
     <form>
@@ -8,18 +7,29 @@
         </div>
     </form><br>
     </div>
+    <div></div>
     <ul class="list-group">
         @if($users && $users->count() > 0)
-        @foreach($users as $user)
-        <a class="list-group-item grid text-white text-3xl text-center max-w-md mx-auto bg-[#f87171] rounded-xl shadow-md overflow-hidden md:max-w-2xl m-5 p-5" href="{{ route('profile', $user) }}"><img
-            src="{{ asset('storage') . '/pfp/' . $user->profile_pic }}"
-            class=" w-1/6  rounded-full transition-all border-secu border-solid border-4"
-            alt="profile-pic">{{$user->name}}</a>
-        @endforeach
+            @foreach($users as $user)
+                <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center text-[#E95F4A] text-3xl text-center max-w-md mx-auto bg-white drop-shadow-lg rounded-xl shadow-md overflow-hidden md:max-w-2xl m-5 p-5">
+                    <div class="sm:col-auto">
+                        <img src="{{ asset('storage/pfp/' . $user->profile_pic) }}" class="w-24 h-24 md:w-48 rounded-full transition-all border-secu border-solid border-4" alt="profile-pic">
+                    </div>
+                    <a class="list-group-item col-span-2 sm:col-auto text-left" href="{{ route('profile', $user) }}">
+                        {{$user->name}}
+                    </a>
+                    <p class="col-span-2 sm:col-auto text-slate-500 ml-1 text-md tracking-wide text-left"></p>
+                </div>
+            @endforeach
         @else
-        <p class="text-info"></p>
+            <p class="text-info"></p>
         @endif
     </ul>
 </div> 
-
+<script>
+    AOS.init({
+    duration: 1000, // duración de la animación (en milisegundos)
+    offset: 200, // offset desde el borde superior de la ventana (en píxeles)
+  });
+  </script>
 
